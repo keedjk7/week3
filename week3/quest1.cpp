379e4878621cb485b5cf948c4bce71b2857b04c3
JIRAPAT KAEWSONGSANG <63010139@KMITL.AC.TH>
2020-08-27 19:56:07 +0700

    none

--- week3/quest1.cpp
+++ week3/quest1.cpp
@@ -1,11 +1,73 @@
-#include<stdio.h>
+#include <iostream>
+#include<string.h>
+#include <ctype.h>
 
-int main() {
-	char Keep[50][50];
-	int input;
-	scanf_s("%d", &input);
-	for (int i = 0; i < input; i++) {
-		scanf_s("%s", Keep[i], 50);
-	}
+using namespace std;
 
+const int MAX_CHAR = 50;
+
+struct leaf {
+    int index;
+    leaf* child[MAX_CHAR];
+    
+    leaf()
+    {
+        for (int i = 0; i < MAX_CHAR; i++) {
+            child[i] = NULL;
+        }
+        index = -1;
+    }
+};
+
+void sort(leaf* root, string str, int index){
+    leaf* node = root;
+    for (int i = 0; i < str.size(); i++) {
+
+        char C = tolower(str[i]) - 'a';
+
+        if (!node->child[C]) {
+            node->child[C] = new leaf();
+        }
+        node = node->child[C];
+    }
+    node->index = index;
+}
+
+bool print(leaf* node, string Keep[])
+{
+    if (node == NULL) {
+        return false;
+    }
+
+    for (int i = 0; i < MAX_CHAR; i++) {
+        if (node->child[i] != NULL) {
+            if (node->child[i]->index != -1) {
+                cout << Keep[node->child[i]->index] << endl;
+            }
+            print(node->child[i], Keep);
+        }
+    }
+}
+
+void sort_print(string Keep[], int input)
+{
+    leaf* root = new leaf();
+
+    for (int i = 0; i < input; i++) {
+        sort(root, Keep[i], i);
+    }
+    printf("\nSort by dictionary\n");
+    print(root, Keep);
+}
+int main()
+{
+    string Keep[1000];
+    int input;
+    printf("Number Of Input String : ");
+    cin >> input;
+    for (int i = 0; i < input; i++) {
+        cin >> Keep[i];
+    }
+    sort_print(Keep, input);
+    return 0;
 }
